8eb8c28c251d70c8529a2785ea45e19466f3d52e
Marek Zając <contact@zajacmarek.com>
2020-02-23 10:33:09 +0100

    Lesson 5.4

---
+++ modules/lessons/App_5_4/App_5_4/Program.cs
@@ -0,0 +1,15 @@
+﻿// TUTAJ DODALIŚMY NAMESPACE
+using App_5_4.Test;
+using System;
+
+namespace App_5_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new MyClass();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
