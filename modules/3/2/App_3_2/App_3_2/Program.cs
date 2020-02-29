bd79779e122dd791cff3558eefc1f80b0e3c5506
Marek Zajac <contact@zajacmarek.com>
2020-02-29 07:34:26 +0100

    Module 3 and 5

---
+++ modules/3/2/App_3_2/App_3_2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace App_3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(/* To jest przywitanie */ "Hello World!");
+            Console.WriteLine("Cześć! Jestem Twoim pierwszym programem!");
+        }
+    }
+}
